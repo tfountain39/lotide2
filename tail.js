8611b716b6763534db2cc397163d273b06eb2cd0
Todd <todd.fountain39@gmail.com>
2023-10-16 20:01:24 -0500

    Update of additional assertions and Linting

--- tail.js
+++ tail.js
@@ -7,10 +7,20 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(arr) {
-  return arr.slice(1);
+  if (arr.length <= 1) {
+    return [];
+  } else {
+    return arr.slice(1);
+  }
 };
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const result2 = tail(["Hello"]);
+assertEqual(result2.length, 0); // an empty array
+
+const result3 = tail([]);
+assertEqual(result3[0], undefined); // ensure we get undefined
