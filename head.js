874d9d75c7663fcba6fdaef9169cfe131b6bff92
Todd <todd.fountain39@gmail.com>
2023-10-16 19:20:32 -0500

    Redo head

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if(actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Helo");
